d87429d62d702e93768d35e04152f292ad850373
Ashok <ashokitschool@gmail.com>
2024-02-07 20:00:51 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpringBoot_Security_Register_Login
---
+++ src/main/java/in/ashokit/rest/CustomerRestController.java
@@ -0,0 +1,67 @@
+package in.ashokit.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.ashokit.entity.Customer;
+import in.ashokit.repo.CustomerRepo;
+
+@RestController
+public class CustomerRestController {
+
+	@Autowired
+	private CustomerRepo crepo;
+
+	@Autowired
+	private PasswordEncoder pwdEncoder;
+
+	@Autowired
+	private AuthenticationManager authManager;
+
+	@PostMapping("/login")
+	public ResponseEntity<String> loginCheck(@RequestBody Customer c) {
+		
+		UsernamePasswordAuthenticationToken token = 
+				new UsernamePasswordAuthenticationToken(c.getUname(), c.getPwd());
+
+		try {
+			Authentication authenticate = authManager.authenticate(token);
+
+			if (authenticate.isAuthenticated()) {
+				return new ResponseEntity<>("Welcome To Ashok IT", HttpStatus.OK);
+			}
+
+		} catch (Exception e) {
+			//logger
+		}
+
+		return new ResponseEntity<String>("Invalid Credentials", HttpStatus.BAD_REQUEST);
+	}
+
+	@PostMapping("/register")
+	public String registerCustomer(@RequestBody Customer customer) {
+		
+		// duplicate check
+
+		String encodedPwd = pwdEncoder.encode(customer.getPwd());
+		customer.setPwd(encodedPwd);
+
+		crepo.save(customer);
+
+		return "User registered";
+	}
+
+}
+
+
+
+
+
